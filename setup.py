dbce5d2978c40429ea8a4fb3bcb4ff1007b0fc77
HarkonenBade <github@harkonen.net>
2022-02-18 16:07:01 +0000

    Typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='KICAD Autogen',
       version='1.0',
       description='KICAD Automatic Generation Scripts',
-      author='Thomsa Bytheway',
+      author='Thomas Bytheway',
       author_email='kicad_autogen@harkonen.net',
       install_requires=['PyYaml>=1.0'],
       scripts=['build_lib.py'],
